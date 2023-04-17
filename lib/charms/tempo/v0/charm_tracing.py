ee575400ed3f351ed75f4f00a086eabc06e50406
Pietro Pasotti <starfire.daemon@gmail.com>
2023-04-17 12:19:18 +0200

    charm tracing lib and tests

---
+++ lib/charms/tempo/v0/charm_tracing.py
@@ -0,0 +1,203 @@
+import functools
+import inspect
+import logging
+from contextlib import contextmanager
+from contextvars import ContextVar
+from io import StringIO
+from typing import Union, Type, Callable, TypeVar, Optional
+
+import opentelemetry
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider, Span
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.trace import get_current_span as otlp_get_current_span, get_tracer_provider
+from ops.charm import CharmBase
+from ops.framework import Framework
+
+logger = logging.getLogger('tracing')
+
+tracer: ContextVar[opentelemetry.trace.Tracer] = ContextVar("tracer")
+# redefine it here to expose it as a toplevel module name
+get_current_span = otlp_get_current_span
+
+
+def _get_tracer() -> Optional[opentelemetry.trace.Tracer]:
+    try:
+        return tracer.get()
+    except LookupError:
+        return None
+
+
+@contextmanager
+def _span(name: str) -> Optional[Span]:
+    """Context to create a span if there is a tracer, otherwise do nothing."""
+    if tracer := _get_tracer():
+        with tracer.start_as_current_span(name) as span:
+            yield span
+    else:
+        yield None
+
+
+PYDEPS = [
+    "opentelemetry"
+]
+
+_C = TypeVar("_C", bound=Type[CharmBase])
+_T = TypeVar("_T", bound=type)
+_F = TypeVar("_F", bound=Type[Callable])
+
+
+class TracingError(RuntimeError):
+    """Base class for errors raised by this module."""
+
+
+class UntraceableObject(TracingError):
+    """Raised when an object you're attempting to instrument cannot be autoinstrumented."""
+
+
+def _setup_root_span_initializer(charm: Type[CharmBase],
+                                 tempo_endpoint: str,
+                                 app_name: Optional[str] = None
+                                 ):
+    """Patches the charm's initializer."""
+    original_init = charm.__init__
+
+    @functools.wraps(original_init)
+    def wrap_init(self: CharmBase, framework: Framework, *args, **kwargs):
+        original_event_context = framework._event_context
+
+        logging.debug('Initializing opentelemetry tracer...')
+        service_name = app_name or charm.__name__
+
+        resource = Resource.create(attributes={
+            "service.name": service_name,
+            "compose_service": service_name
+        })
+        provider = TracerProvider(resource=resource)
+
+        tempo = getattr(self, tempo_endpoint)
+        if not tempo:
+            logger.warning(f'{charm}.{tempo_endpoint} returned {tempo}; '
+                           f'continuing with tracing DISABLED.')
+            return original_init(self, framework, *args, **kwargs)
+
+        elif not isinstance(tempo, str):
+            raise TypeError(
+                f'{charm}.{tempo_endpoint} should return a tempo endpoint (string); '
+                f'got {tempo} instead.'
+            )
+        else:
+            logger.debug(f'Setting up span exporter to endpoint: {tempo}')
+            exporter = OTLPSpanExporter(endpoint=tempo)
+
+        processor = BatchSpanProcessor(exporter)
+        provider.add_span_processor(processor)
+        opentelemetry.trace.set_tracer_provider(provider)
+        _tracer = opentelemetry.trace.get_tracer(service_name)
+
+        span = _tracer.start_span('charm exec')
+
+        _tracer_token = tracer.set(_tracer)
+
+        @contextmanager
+        def wrap_event_context(event_name: str):
+            # when the framework enters an event context, we create a span.
+            with _span("event: " + event_name) as span:
+                if span:
+                    # todo: figure out how to inject event attrs in here
+                    span.add_event(event_name)
+                yield original_event_context(event_name)
+
+        framework._event_context = wrap_event_context
+
+        original_close = framework.close
+
+        @functools.wraps(original_close)
+        def wrap_close():
+            span.end()
+            tracer.reset(_tracer_token)
+            tp: TracerProvider = get_tracer_provider()
+            tp.force_flush()
+            tp.shutdown()
+            original_close()
+
+        framework.close = wrap_close
+
+        return original_init(self, framework, *args, **kwargs)
+
+    charm.__init__ = wrap_init
+
+
+_notgiven = object()
+
+
+def trace_charm(tempo_endpoint: str, app_name: str = None) -> Callable[[_C], _C]:
+    """Setup tracing on this charm class."""
+
+    def trace_charm(charm: _C) -> _C:
+        """Prepares the charm as tracing root: when the charm is run, the root span will open."""
+
+        logger.info(f'instrumenting {charm}')
+        # check that it is in dir(charm).
+        tempo = getattr(charm, tempo_endpoint, _notgiven)
+        if tempo is _notgiven:
+            raise RuntimeError(f'you passed tempo_endpoint={tempo_endpoint} to '
+                               f'@trace_charm; but {charm}.{tempo_endpoint} was not found.')
+
+        _setup_root_span_initializer(charm, tempo_endpoint, app_name=app_name)
+        trace_type(charm)
+        return charm
+
+    return trace_charm
+
+
+def trace_type(cls: _T) -> _T:
+    logger.info(f'instrumenting {cls}')
+    for name, method in inspect.getmembers(cls, predicate=inspect.isfunction):
+        logger.info(f'discovered {method}')
+
+        if method.__name__.startswith("__"):
+            logger.info(f"skipping (dunder)")
+            continue
+
+        setattr(cls, name, trace_function(method))
+
+    return cls
+
+
+def trace_function(function: _F) -> _F:
+    logger.info(f'instrumenting {function}')
+    # sig = inspect.signature(function)
+    @functools.wraps(function)
+    def wrapped_function(*args, **kwargs):
+        with _span("method call: " + function.__name__):
+            return function(*args, **kwargs)
+
+    # wrapped_function.__signature__ = sig
+    return wrapped_function
+
+
+def trace(obj: Union[Type, Callable]):
+    """Decorator to trace an object and send the resulting spans to Tempo."""
+    if isinstance(obj, type):
+        return trace_type(obj)
+    else:
+        try:
+            return trace_function(obj)
+        except:
+            raise UntraceableObject(
+                f'cannot create span from {type(obj)}; instrument {obj} manually.'
+            )
+
+
+_ENABLED = False
+
+
+def enable():
+    global _ENABLED
+    _ENABLED = True
+
+
+def enabled():
+    return _ENABLED
---
+++ tests/unit/test_charm_tracing.py
@@ -0,0 +1,192 @@
+import sys
+from unittest.mock import patch
+
+import pytest
+from ops import EventBase, EventSource
+from ops.charm import CharmBase, CharmEvents
+from scenario import State
+
+from charms.tempo.v0.charm_tracing import trace_charm, trace, get_current_span
+
+
+@pytest.fixture(autouse=True)
+def cleanup():
+    def patched_set_tracer_provider(tracer_provider, log):
+        import opentelemetry
+        opentelemetry.trace._TRACER_PROVIDER = tracer_provider
+
+    with patch("opentelemetry.trace._set_tracer_provider",
+               new=patched_set_tracer_provider):
+        yield
+
+
+@trace_charm("tempo")
+class MyCharmSimple(CharmBase):
+    META = {'name': "frank"}
+
+    @property
+    def tempo(self):
+        return "foo.bar:80"
+
+
+def test_base_tracer_endpoint(caplog):
+    import opentelemetry
+    with patch("opentelemetry.exporter.otlp.proto.grpc.exporter.OTLPExporterMixin._export") as f:
+        f.return_value = opentelemetry.sdk.metrics._internal.export.MetricExportResult.SUCCESS
+        State().trigger('start', MyCharmSimple,
+                        meta=MyCharmSimple.META)
+        assert "Setting up span exporter to endpoint: foo.bar:80" in caplog.text
+        span = f.call_args_list[0].args[0][0]
+        assert span.resource.attributes['service.name'] == 'MyCharmSimple'
+        assert span.resource.attributes['compose_service'] == 'MyCharmSimple'
+
+
+@trace_charm("tempo")
+class MyCharmSimpleDisabled(CharmBase):
+    META = {'name': "frank"}
+
+    @property
+    def tempo(self):
+        return None
+
+
+def test_base_tracer_endpoint_disabled(caplog):
+    import opentelemetry
+    with patch("opentelemetry.exporter.otlp.proto.grpc.exporter.OTLPExporterMixin._export") as f:
+        f.return_value = opentelemetry.sdk.metrics._internal.export.MetricExportResult.SUCCESS
+        State().trigger('start', MyCharmSimpleDisabled,
+                        meta=MyCharmSimpleDisabled.META)
+
+        assert "continuing with tracing DISABLED." in caplog.text
+        assert not f.called
+
+
+@trace
+def _my_fn(foo):
+    return foo + 1
+
+
+@trace_charm("tempo")
+class MyCharmSimpleEvent(CharmBase):
+    META = {'name': "frank"}
+
+    def __init__(self, fw):
+        super().__init__(fw)
+        fw.observe(self.on.start, self._on_start)
+        span = get_current_span()
+        span.add_event("log", {
+            "foo": "bar",
+            "baz": "qux",
+        })
+
+    def _on_start(self, _):
+        _my_fn(2)
+
+    @property
+    def tempo(self):
+        return "foo.bar:80"
+
+
+def test_base_tracer_endpoint_event(caplog):
+    import opentelemetry
+    with patch("opentelemetry.exporter.otlp.proto.grpc.exporter.OTLPExporterMixin._export") as f:
+        f.return_value = opentelemetry.sdk.metrics._internal.export.MetricExportResult.SUCCESS
+        State().trigger('start', MyCharmSimpleEvent,
+                        meta=MyCharmSimpleEvent.META)
+
+        spans = f.call_args_list[0].args[0]
+        span0, span1, span2, span3 = spans
+        assert span0.name == 'method call: _my_fn'
+
+        assert span1.name == 'method call: _on_start'
+
+        assert span2.name == 'event: start'
+        evt = span2.events[0]
+        assert evt.name == 'start'
+
+        assert span3.name == 'charm exec'
+
+        for span in spans:
+            assert span.resource.attributes['service.name'] == 'MyCharmSimpleEvent'
+
+
+@trace_charm("tempo")
+class MyCharmWithMethods(CharmBase):
+    META = {'name': "frank"}
+
+    def __init__(self, fw):
+        super().__init__(fw)
+        fw.observe(self.on.start, self._on_start)
+
+    def _on_start(self, _):
+        self.a()
+        self.b()
+        self.c()
+
+    def a(self):
+        pass
+
+    def b(self):
+        pass
+
+    def c(self):
+        pass
+
+    @property
+    def tempo(self):
+        return "foo.bar:80"
+
+
+def test_base_tracer_endpoint_methods(caplog):
+    import opentelemetry
+    with patch("opentelemetry.exporter.otlp.proto.grpc.exporter.OTLPExporterMixin._export") as f:
+        f.return_value = opentelemetry.sdk.metrics._internal.export.MetricExportResult.SUCCESS
+        State().trigger('start', MyCharmWithMethods,
+                        meta=MyCharmWithMethods.META)
+
+        spans = f.call_args_list[0].args[0]
+        span_names = [span.name for span in spans]
+        assert span_names == ['method call: a', 'method call: b', 'method call: c', 'method call: _on_start',
+                              "event: start", 'charm exec']
+
+
+
+class Foo(EventBase):
+    pass
+
+class MyEvents(CharmEvents):
+    foo = EventSource(Foo)
+
+@trace_charm("tempo")
+class MyCharmWithCustomEvents(CharmBase):
+    on = MyEvents()
+
+    META = {'name': "frank"}
+    def __init__(self, fw):
+        super().__init__(fw)
+        fw.observe(self.on.start, self._on_start)
+        fw.observe(self.on.foo, self._on_foo)
+
+    def _on_start(self, _):
+        self.on.foo.emit()
+
+    def _on_foo(self, _):
+        pass
+
+    @property
+    def tempo(self):
+        return "foo.bar:80"
+
+def test_base_tracer_endpoint_custom_event(caplog):
+    import opentelemetry
+    with patch("opentelemetry.exporter.otlp.proto.grpc.exporter.OTLPExporterMixin._export") as f:
+        f.return_value = opentelemetry.sdk.metrics._internal.export.MetricExportResult.SUCCESS
+        State().trigger('start', MyCharmWithCustomEvents,
+                        meta=MyCharmWithCustomEvents.META)
+
+        spans = f.call_args_list[0].args[0]
+        span_names = [span.name for span in spans]
+        assert span_names == ['method call: _on_foo', 'event: foo',
+                              'method call: _on_start', 'event: start', 'charm exec']
+
+
