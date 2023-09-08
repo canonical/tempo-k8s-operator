60d332700365c2e4faaff6730cb877030e9fd12d
PietroPasotti <starfire.daemon@gmail.com>
2023-09-08 14:59:20 +0200

    Charm tracing name revert (#43)

--- tests/scenario/test_charm.py
+++ tests/scenario/test_charm.py
@@ -3,7 +3,7 @@ import socket
 from pathlib import Path
 
 import pytest
-from charms.tempo_k8s.v0.charm_instrumentation import _charm_tracing_disabled
+from charms.tempo_k8s.v0.charm_tracing import charm_tracing_disabled
 from scenario import Relation, State
 from scenario.sequences import check_builtin_sequences
 
@@ -11,7 +11,7 @@ TEMPO_CHARM_ROOT = Path(__file__).parent.parent.parent
 
 
 def test_builtin_sequences(tempo_charm):
-    with _charm_tracing_disabled():
+    with charm_tracing_disabled():
         check_builtin_sequences(tempo_charm)
 
 
@@ -22,7 +22,7 @@ def base_state(request):
 
 def test_start(context, base_state):
     # verify the charm runs at all with and without leadership
-    with _charm_tracing_disabled():
+    with charm_tracing_disabled():
         context.run("start", base_state)
 
 
@@ -30,7 +30,7 @@ def test_tempo_endpoint_published(context):
     tracing = Relation("tracing")
     state = State(leader=True, relations=[tracing])
 
-    with _charm_tracing_disabled():
+    with charm_tracing_disabled():
         out = context.run(tracing.created_event, state)
 
     tracing_out = out.get_relations(tracing.endpoint)[0]
