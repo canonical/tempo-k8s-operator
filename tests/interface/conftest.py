16a8e4befcd8f22d106924c9c08f4f917184b1e1
Pietro Pasotti <starfire.daemon@gmail.com>
2024-02-14 12:58:15 +0100

    point to cri main

--- tests/interface/conftest.py
+++ tests/interface/conftest.py
@@ -21,7 +21,6 @@ def interface_tester(interface_tester: InterfaceTester):
     with patch("charm.KubernetesServicePatch"):
         with charm_tracing_disabled():
             interface_tester.configure(
-                branch="tracing-v2",  # todo remove when CRI:tracing-v2 is merged
                 charm_type=TempoCharm,
                 state_template=State(
                     leader=True,
