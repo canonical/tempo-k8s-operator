3c3cd813ba5e50f5c2093ec58b3ce8ea81149f50
Pietro Pasotti <starfire.daemon@gmail.com>
2024-04-12 15:32:19 +0200

    repro steps and tracegen script

---
+++ tracegen.py
@@ -0,0 +1,31 @@
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
+    OTLPSpanExporter,
+)
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+)
+
+span_exporter = OTLPSpanExporter(
+    endpoint="http://10.1.232.162:4317",
+    # endpoint="http://10.1.232.162:8080/otlp_grpc",
+    insecure=True,
+)
+resource = Resource.create(attributes={"service.name": "tracegen"})
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(ConsoleSpanExporter())
+span_processor = BatchSpanProcessor(span_exporter)
+provider.add_span_processor(processor)
+provider.add_span_processor(span_processor)
+trace.set_tracer_provider(provider)
+
+
+tracer = trace.get_tracer(__name__)
+
+with tracer.start_as_current_span("foo"):
+    with tracer.start_as_current_span("bar"):
+        with tracer.start_as_current_span("baz"):
+            print("Hello world from OpenTelemetry Python!")
