80df792257db4dbbe3f4d54ebaec6bdf4ec0815f
Mateusz Kulewicz <mateusz.kulewicz@canonical.com>
2024-04-22 13:47:38 +0200

    Add separate tester for GRPC charms - there can't be two TracerProvider instances in one context

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -11,6 +11,8 @@ METADATA = yaml.safe_load(Path("./charmcraft.yaml").read_text())
 APP_NAME = METADATA["name"]
 TESTER_METADATA = yaml.safe_load(Path("./tests/integration/tester/metadata.yaml").read_text())
 TESTER_APP_NAME = TESTER_METADATA["name"]
+TESTER_GRPC_METADATA = yaml.safe_load(Path("./tests/integration/tester-grpc/metadata.yaml").read_text())
+TESTER_GRPC_APP_NAME = TESTER_GRPC_METADATA["name"]
 
 logger = logging.getLogger(__name__)
 
@@ -18,14 +20,16 @@ logger = logging.getLogger(__name__)
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest):
     # Given a fresh build of the charm
-    # When deploying it together with the tester
+    # When deploying it together with testers
     # Then applications should eventually be created
     tempo_charm = await ops_test.build_charm(".")
     tester_charm = await ops_test.build_charm("./tests/integration/tester/")
+    tester_grpc_charm = await ops_test.build_charm("./tests/integration/tester-grpc/")
     resources = {
         "tempo-image": METADATA["resources"]["tempo-image"]["upstream-source"],
     }
     resources_tester = {"workload": TESTER_METADATA["resources"]["workload"]["upstream-source"]}
+    resources_tester_grpc = {"workload": TESTER_GRPC_METADATA["resources"]["workload"]["upstream-source"]}
 
     await asyncio.gather(
         ops_test.model.deploy(tempo_charm, resources=resources, application_name=APP_NAME),
@@ -33,6 +37,12 @@ async def test_build_and_deploy(ops_test: OpsTest):
             tester_charm,
             resources=resources_tester,
             application_name=TESTER_APP_NAME,
+            num_units=3,
+        ),
+        ops_test.model.deploy(
+            tester_grpc_charm,
+            resources=resources_tester_grpc,
+            application_name=TESTER_GRPC_APP_NAME,
             num_units=3,
         ),
     )
@@ -49,6 +59,9 @@ async def test_build_and_deploy(ops_test: OpsTest):
         ops_test.model.wait_for_idle(
             apps=[TESTER_APP_NAME], raise_on_blocked=True, timeout=1000, raise_on_error=False
         ),
+        ops_test.model.wait_for_idle(
+            apps=[TESTER_GRPC_APP_NAME], raise_on_blocked=True, timeout=1000, raise_on_error=False
+        ),
     )
 
     assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
@@ -60,8 +73,9 @@ async def test_relate(ops_test: OpsTest):
     # when relating it together with the tester
     # then relation should appear
     await ops_test.model.add_relation(APP_NAME + ":tracing", TESTER_APP_NAME + ":tracing-v2")
+    await ops_test.model.add_relation(APP_NAME + ":tracing", TESTER_GRPC_APP_NAME + ":tracing-v2")
     await ops_test.model.wait_for_idle(
-        apps=[APP_NAME, TESTER_APP_NAME],
+        apps=[APP_NAME, TESTER_APP_NAME, TESTER_GRPC_APP_NAME],
         status="active",
         timeout=1000,
     )
@@ -96,8 +110,9 @@ async def test_verify_traces_http(ops_test: OpsTest):
     assert found, f"There's no trace of charm exec traces in tempo. {json.dumps(traces, indent=2)}"
 
 
+@pytest.mark.abort_on_fail
 async def test_verify_traces_grpc(ops_test: OpsTest):
-    # the tester charm emits a single grpc trace in its common exit hook
+    # the tester-grpc charm emits a single grpc trace in its common exit hook
     # we verify it's there
     status = await ops_test.model.get_status()
     app = status["applications"][APP_NAME]
@@ -118,7 +133,7 @@ async def test_verify_traces_grpc(ops_test: OpsTest):
 
     found = False
     for trace in traces:
-        if trace["rootServiceName"] == "tempo-tester-tracegen":
+        if trace["rootServiceName"] == "TempoTesterGrpcCharm":
             found = True
 
     assert (
@@ -132,10 +147,12 @@ async def test_remove_relation(ops_test: OpsTest):
     # when relation is removed
     # then both charms should become active again
     await ops_test.juju("remove-relation", APP_NAME + ":tracing", TESTER_APP_NAME + ":tracing-v2")
+    await ops_test.juju("remove-relation", APP_NAME + ":tracing", TESTER_GRPC_APP_NAME + ":tracing-v2")
     await asyncio.gather(
         ops_test.model.wait_for_idle(
             apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
         ),
         # for tester, depending on the result of race with tempo it's either waiting or active
         ops_test.model.wait_for_idle(apps=[TESTER_APP_NAME], raise_on_blocked=True, timeout=1000),
+        ops_test.model.wait_for_idle(apps=[TESTER_GRPC_APP_NAME], raise_on_blocked=True, timeout=1000),
     )
---
+++ tests/integration/tester-grpc/README.md
@@ -0,0 +1,2 @@
+to deploy:
+`juju deploy /path/to/tester.charm --resource workload=python:slim-buster --config tempo_endpoint:foo.bar:4318`
