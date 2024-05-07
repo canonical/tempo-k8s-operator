956e0041c4080b0044edfbe47bcb9043806f960e
Mateusz Kulewicz <mateusz.kulewicz@canonical.com>
2024-05-07 21:39:02 +0400

    unit test fix

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -32,6 +32,7 @@ class TestTempoCharm(unittest.TestCase):
         expected_entrypoints = {
             "entryPoints": {
                 "tempo-http": {"address": ":3200"},
+                "tempo-grpc": {"address": ":9096"},
                 "zipkin": {"address": ":9411"},
                 "otlp-grpc": {"address": ":4317"},
                 "otlp-http": {"address": ":4318"},
