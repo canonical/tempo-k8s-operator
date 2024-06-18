2fae0089c47d1af264a4d62052bbff6746f4a591
Pietro Pasotti <starfire.daemon@gmail.com>
2024-06-18 10:04:29 +0200

    lint

--- lib/charms/tempo_k8s/v1/charm_tracing.py
+++ lib/charms/tempo_k8s/v1/charm_tracing.py
@@ -146,7 +146,7 @@ LIBAPI = 1
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
 
-LIBPATCH = 8
+LIBPATCH = 9
 
 PYDEPS = ["opentelemetry-exporter-otlp-proto-http==1.21.0"]
 
